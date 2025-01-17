746c374fca9c009541a9592e35f293e1ff84db92
Ikechukwu Sydney Aghanchi-Quality Assurance Tester <iaghanchi@Accion.local>
2025-01-17 14:37:16 +0100

    This is an update on the previous code pushed as at 1/17/2025

--- src/test/java/Page_Object.java
+++ src/test/java/Page_Object.java
@@ -1,8 +1,27 @@
 public class Page_Object {
 
+    public String name;
+    public String city;
+
+    public Page_Object(String name, String city){
+        this.name = name;
+        this.city = city;
+    }
+     public String SetName(String name){
+        return name;
+     }
+     public String SetCollege(){
+        return city;
+     }
+    public void getName(String name){
+        this.name = name;
+    }
+    public void getCity(String city){
+        this.city = city;
+    }
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            // Your code here
-        }
+     Page_Object name = new Page_Object("", "");
+      name.SetName("Calabar");
+        System.out.println(name.SetName("John" + " This is my name and i love it from my heart thanks to my mom"));
     }
 }
